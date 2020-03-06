f49bd5dd46b70acfdfec1461898cef170345b657
MayankValechha <valechhamayank12@gmail.com>
2020-03-06 22:00:09 +0530

    New App for Handling Users

---
+++ users/views.py
@@ -0,0 +1,16 @@
+from django.shortcuts import render, redirect
+from .forms import UserRegisterForm
+from django.contrib import messages
+
+
+def register(request):
+    if request.method == 'POST':
+        form = UserRegisterForm(request.POST)
+        if form.is_valid():
+            form.save()
+            username = form.cleaned_data.get('username')
+            messages.success(request, f'Your account has been created. You can now able to login!')
+            return redirect('register')
+    else:
+        form = UserRegisterForm()
+    return render(request, 'users/register.html', {'form' : form})
